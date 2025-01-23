80c3d8e34d4998e1cb40f569417f44bd074be5e0
Muhammad Wildan Fathur Rahman <muhammad.wildan.fr81@gmail.com>
2025-01-23 21:46:06 +0700

    first init

---
+++ app/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
+const app = express();
+require("dotenv").config;
+
+app.set("view engine", "ejs");
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", { weather: null, error: null });
+});
+
+app.get("/weather", async (req, res) => {
+  const city = req.query.city;
+  const apiKey = "f06d448d3d6589126f21fd9d5e18c196";
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  let weather;
+  let error = null;
+
+  try {
+    const response = await axios.get(apiURL);
+    weather = response.data;
+  } catch {
+    weather = null;
+    error = "error, coba lagi";
+  }
+  res.render("index", { weather, error });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`server berjalan di port ${port}`);
+});
